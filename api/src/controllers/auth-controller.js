150fed8792bb830e299b0a56f7f7386b0772f112
Daniel Limas <amedlimas97@gmail.com>
2020-11-06 18:35:40 -0700

    Modified the verify token function

--- api/src/controllers/auth-controller.js
+++ api/src/controllers/auth-controller.js
@@ -1,7 +1,25 @@
-const User = require('../models/Auth')
+const Auth = require('../models/Auth')
 const jwt = require('jsonwebtoken')
 
-async function signIn(req, res){
+async function signup(req, res){
+    console.log('Body of the signup request:')
+    console.log(req.body)
+    const { username, pin } = req.body
+    const auth = Auth.findOne({username: username})
+    if(auth){
+        console.log({msg: "That username already exists!"})
+        return res.status(401).send({msg: 'Username already exists!'})
+    }else{
+        const newAuth = new Auth({username, pin})
+        await newAuth.save()
+        console.log({msg: `The user ${newAuth.username} has been created`})
+        return res.status(200).send({msg: {msg: `The user ${newAuth.username} has been created`}})
+    }
+}
+
+async function signin(req, res){
+    console.log('Body of the login request:')
+    console.log(req.body)
 
 }
 
@@ -13,9 +31,26 @@ function logout(req, res){
 
 }
 
+function verifyToken(){
+    let token = req.headers['x-access-token']
+    //console.log(token)
+    if(!token){
+        res.status(401).send({auth: false, message: 'No token provided'})
+    }else{
+        jwt.verify(token, "supersecretsecret", async (err, decoded) => {
+            if(err){
+                return false
+            }else{
+                console.log({auth: true, message: "Token validated!"})
+                return true
+            }
+        })
+    }
+}
 
 module.exports = {
-    signIn,
+    signin,
+    signup,
     newPin,
     logout
 }
