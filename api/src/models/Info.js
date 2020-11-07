253ba72aa78fdbb6b6e0c8f9f6234f6518235101
Daniel Limas <amedlimas97@gmail.com>
2020-11-07 00:19:38 -0700

    Info model

---
+++ api/src/models/Info.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const InfoSchema = new Schema({
+    peopleEntering: {type: Number, isRequired: true},
+    peopleInside: {type: Number, isRequired: true},
+    store: {type: String, isRequired: true},
+    timestamp: {type: Date, isRequired: true},
+})
+
+module.exports = mongoose.model("Info", InfoSchema)
